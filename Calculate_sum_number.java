03c7dc228dd8bf5bd89760379792762dedcd17ea
rajkanya-chaudhary <dc5068786@gmail.com>
2024-05-15 23:30:48 +0530

    this is a factor of number using for loop

---
+++ Calculate_sum_number.java
@@ -0,0 +1,16 @@
+public class Calculate_sum_number {
+    public static void main(String[] args) {
+        
+       int arr[] = {12, 13, 1, 10, 34, 10};
+
+       int sum = 0;
+
+       for(int i=0; i<arr.length; i++)
+       {
+         sum = sum + arr[i];
+       }
+
+       System.out.print(sum); 
+   }
+}
+
---
+++ Factor_of_number.java
@@ -0,0 +1,15 @@
+public class Factor_of_number {
+    public static void main(String[] args) {
+        int num = 10;
+
+        System.out.println( "Factors of " + num + " are " );
+   
+        // finding and printing factors b/w 1 to num
+        for(int i = 1; i <= num; i++)
+        {
+            if(num % i == 0)
+                System.out.println(i + " "); 
+        }
+   
+    }
+}
